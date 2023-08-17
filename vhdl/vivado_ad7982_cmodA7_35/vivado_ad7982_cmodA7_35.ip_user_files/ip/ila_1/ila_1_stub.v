a44ee5c5db2fd2c8b1853c8a9de1f14925650f04
bbertrand-eng <bernard.bertrand@irap.omp.eu>
2023-08-17 16:07:36 +0200

    ajout fichier dcp necessaire à la synthese

--- vhdl/vivado_ad7982_cmodA7_35/vivado_ad7982_cmodA7_35.ip_user_files/ip/ila_1/ila_1_stub.v
+++ vhdl/vivado_ad7982_cmodA7_35/vivado_ad7982_cmodA7_35.ip_user_files/ip/ila_1/ila_1_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
-// Date        : Fri Aug 11 14:50:15 2023
+// Date        : Thu Aug 17 15:29:13 2023
 // Host        : DESKTOP-BSP8Q2B running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
 //               c:/3u/ad7982_cmodA7_35/vhdl/vivado_ad7982_cmodA7_35/vivado_ad7982_cmodA7_35.gen/sources_1/ip/ila_1/ila_1_stub.v
